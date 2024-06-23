517400d660dcebe1b3db7b3194b59e15b29cabd5
tagche <l.tagche@gmail.com>
2024-06-23 18:25:31 +0900

    Vercel Analytics実装

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 'use client';
 
+import { RecoilRoot } from "recoil";
+import { Analytics } from "@vercel/analytics/react"
 import { Noto_Sans_JP } from "next/font/google";
 import Footer from "./components/footer";
 import "../styles/quiz.scss";
-import { RecoilRoot } from "recoil";
 
 const noto = Noto_Sans_JP ({
     weight: ["400", "700", "900"],
@@ -14,6 +15,7 @@ const noto = Noto_Sans_JP ({
 export default function RootLayout({children,}: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="ja">
+            <Analytics />
             <body className={noto.className}>
                 <RecoilRoot>
                     {children}
